bb197b14aaad3a7d7eb1d80c7dd4d5e4fa4f84f6
Jake Sinclair <sinclairs1234@gmail.com>
2024-10-21 16:13:38 +0100

    Fixed pairs.

--- question2.ml
+++ question2.ml
@@ -3,35 +3,37 @@
 (* a (3 marks) *)
 
 (* Provide definitions below! Remember to include comments to justify your solutions. *)
-(*  *)
-type suit = HEARTS | CLUBS | DIAMONDS | SPADES
+type suit = Hearts | Clubs | Diamonds | Spades
+
+type cardValue = Number of int | Ten | Jack | Queen | King | Ace
 
 (* Any number card is of the primitive type int and the face cards can be either
 a jack, queen or king. I have chosen to not implement the joker card commonly 
 found in real packs as they are not typically used. *)
-type card = ACE | NUMBER of int | TEN | JACK | QUEEN | KING
+type card = (suit * cardValue)
 
 (* validSuit : suit -> bool *)
-let validSuit value =
-  match value with
-  | HEARTS -> true
-  | CLUBS -> true
-  | DIAMONDS -> true
-  | SPADES -> true
+let validSuit suit =
+  match suit with
+  | Hearts -> true
+  | Clubs -> true
+  | Diamonds -> true
+  | Spades -> true
 
+let validCardValue cardValue =
+  match cardValue with
+  | Number n -> 1 < n && n < 10
+  | Ten -> true
+  | Jack -> true
+  | Queen -> true
+  | King -> true
+  | Ace -> true
 
 (* validCard : card -> bool *)
 
-(* FIX THIS TO INCLUDE ALL CARDS #########################################*)
-
-let validCard value =
-  match value with
-  | ACE -> true
-  | NUMBER number -> if 1 < number && number < 10 then true else false
-  | JACK -> true
-  | QUEEN -> true
-  | KING -> true
-  | _ -> false
+let validCard card =
+  match card with
+  | (suit, cardValue) -> validSuit suit && validCardValue cardValue
 
 (* b (3 marks) *)
 
